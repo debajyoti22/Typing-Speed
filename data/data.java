5351244d919a937b4e3f0d80869e18b824bbd3e9
debajyoti22 <91139741+debajyoti22@users.noreply.github.com>
2023-01-03 13:24:38 +0530

    Update data.java
    added more data

--- data/data.java
+++ data/data.java
@@ -1,10 +1,13 @@
 package data;
 public class data {
     public static String words[] = {
-        "envelope","cantelope","the","hello","microphone","elephant","biscuit","hammer","went","cap"
-    
-    
+        "envelope","cantelope","the","hello","microphone","elephant","biscuit","hammer","went","cap",
+        "extent","crystal","toll","jurisdiction","lunch","seed","wheat","eavesdrop","firsthand","privacy",
+        "worth","conviction","offender","banana","dead","eat","graduate","forestry","ambiguous","combination",
+        "winner","range","decay","hunting","medal","edition","stress","missile","campaign","crosswalk",
+        "pray","misery","pen","brag","ensure","teenager","dilemma","eliminate","bait","noise"       
     
     };
 
 }
+
